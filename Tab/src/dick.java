84955534567fde1f5a340050ba8ae7362a915dd2
User <User@192.168.0.102>
2016-02-07 20:32:25 +0600

    Operatcii s namber

---
+++ Tab/src/dick.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class dick{
+	
+public static void main(String args[]){
+Rider r = new Rider("ÏÐÈÐÎÄÀ");
+r.setVisible(true);
+r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+r.setSize(215, 165);
+r.setResizable(false);
+r.setLocationRelativeTo(null);
+
+}
+}
